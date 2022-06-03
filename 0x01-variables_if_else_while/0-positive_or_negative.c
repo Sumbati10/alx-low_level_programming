8bca3d966c83e955d71db5e3a3bf00aa0d7a58d6
Sumbati10 <lindashirasala@gmail.com>
2022-06-03 04:23:38 -0700

    task zero print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -21,11 +22,10 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-		else
-		{
-			printf("%d is negative\n", n);
-		}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-		return (0);
-
+	return (0);
 }
