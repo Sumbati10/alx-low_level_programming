299bde5c8bade7e2042a89d1ae62ab78ab52947e
Sumbati10 <lindashirasala@gmail.com>
2022-06-30 05:43:21 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,56 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
- *
- * Return: If new_size == old_size - ptr.
- *          If new_size == 0 and ptr is not NULL - NULL.
- *          Otherwise - a pointer to the reallocated memory block.
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int main(int argc, char *argv[])
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	unsigned long mul;
+	int i, j;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
+	if (argc != 3)
 	{
-		mem = malloc(new_size);
-
-		if (mem == NULL)
-			return (NULL);
-
-		return (mem);
+		printf("Error\n");
+		exit(98);
 	}
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+	for (i = 1; i < argc; i++)
 
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+		}
 
-	filler = mem;
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
 
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
-	free(ptr);
-	return (mem);
+	return (0);
 }
